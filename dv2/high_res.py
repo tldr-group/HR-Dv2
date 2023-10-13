d3728684b607860c78ed558c6cb65c1e01af24f6
rmdocherty <rmdocherty2@gmai.com>
2023-10-13 16:56:21 +0100

    Updated the high_res module to use torch.roll & interpolate (i.e simplify greatly)

--- dv2/high_res.py
+++ dv2/high_res.py
@@ -4,195 +4,128 @@ import torchvision.transforms as transforms
 import torch.nn as nn
 import torch.nn.functional as F
 
-from PIL import Image
-from os import getcwd
-
 from typing import List, Literal, TypeAlias, Tuple
 
 Neighbourhood: TypeAlias = Literal["Neumann", "Moore"]
-PATH = getcwd()
-
-# TRANSFORMS
-IMG_SIZE: int = 518
-RESIZE_DIM: int = IMG_SIZE + 2
-PATCH_SIZE: int = 14
-FEAT_DIM: int = 384  # for vits
-PATCH_W: int = IMG_SIZE // PATCH_SIZE
-PATCH_H: int = IMG_SIZE // PATCH_SIZE
-
-transform = transforms.Compose(
-    [
-        transforms.Resize(RESIZE_DIM),
-        transforms.CenterCrop(IMG_SIZE),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=0.5, std=0.2),
-    ]
-)
-
-unnormalize = transforms.Normalize(mean=-0.5 / 0.2, std=1 / 0.2)
-to_img = transforms.ToPILImage()
 
 
-def load_image(
-    path: str, transform: transforms.Compose
-) -> Tuple[torch.Tensor, Image.Image]:
-    # Load image with PIL, convert to tensor by applying $transform, and invert transform to get display image
-    image = Image.open(path).convert("RGB")
-    tensor = transform(image)
-    transformed_img = to_img(unnormalize(tensor))
-    return tensor, transformed_img
-
-
-# MODULE
-def svd_flip(u, v):
-    # columns of u, rows of v
-    max_abs_cols = torch.argmax(torch.abs(u), 0)
-    i = torch.arange(u.shape[1]).to(u.device)
-    signs = torch.sign(u[max_abs_cols, i])
-    u *= signs
-    v *= signs.view(-1, 1)
-    return u, v
-
-
-class PCA(nn.Module):
-    # from user gngdb https://github.com/gngdb/pytorch-pca/blob/main/pca.py
-    def __init__(self, n_components):
-        super().__init__()
-        self.n_components = n_components
-
-    @torch.no_grad()
-    def fit(self, X):
-        n, d = X.size()
-        if self.n_components is not None:
-            d = min(self.n_components, d)
-        self.register_buffer("mean_", X.mean(0, keepdim=True))
-        Z = X - self.mean_  # center
-        U, S, Vh = torch.linalg.svd(Z, full_matrices=False)
-        Vt = Vh
-        U, Vt = svd_flip(U, Vt)
-        self.register_buffer("components_", Vt[:d])
-        return self
-
-    @torch.no_grad()
-    def forward(self, X):
-        return self.transform(X)
-
-    @torch.no_grad()
-    def transform(self, X):
-        assert hasattr(self, "components_"), "PCA must be fit before use."
-        return torch.matmul(X - self.mean_, self.components_.t())
-
-    @torch.no_grad()
-    def fit_transform(self, X):
-        self.fit(X)
-        return self.transform(X)
-
-    @torch.no_grad()
-    def inverse_transform(self, Y):
-        assert hasattr(self, "components_"), "PCA must be fit before use."
-        return torch.matmul(Y, self.components_) + self.mean_
-
-
-# TODO: replace on set of loops by placing all masks into one kernel
-# also only PCA *after* aggregating rather than before - as post processing step
 class HighResDV2(nn.Module):
     def __init__(
         self,
         dino_name: str,
         shifts: List[int],
         pattern: Neighbourhood = "Neumann",
-        n_components: int = 30,
-        patch_size: int = 14,
+        dtype: torch.dtype = torch.float32,
+        pca_each: bool = False,
     ) -> None:
         super().__init__()
 
         self.dinov2: nn.Module = torch.hub.load("facebookresearch/dinov2", dino_name)
         self.dinov2.eval()
-        masks_tensors = self.compute_masks(pattern)
-        # Make these masks parameters s.t they're moved to GPU
-        self.masks = nn.ParameterList(
-            [nn.Parameter(i, requires_grad=False) for i in masks_tensors]
-        )
-        self.shifts = shifts
-        self.n_components = n_components
-        self.PCA = PCA(n_components)
 
-        self.patch_size: int = patch_size
+        self.shift_directions = self.compute_shift_directions(pattern)
+        self.shift_distances = shifts
 
-    def compute_masks(self, pattern: Neighbourhood) -> List[torch.Tensor]:
-        # Precompute neighbourhoods/weights for 3x3 conv2d
-        out_masks: List[torch.Tensor] = []
+        feat, patch = self.get_model_params(dino_name)
+        self.patch_size: int = patch
+        self.feat_dim: int = feat
+
+        self.dtype = dtype
+
+        self.pca_each = pca_each
+        self.pca_k: int = 70
+
+    def get_model_params(self, dino_name: str) -> Tuple[int, int]:
+        # Match a name like dinov2_vits14 / dinov2_vitg16_lc etc. to feature dim and patch size.
+        split_name = dino_name.split("_")
+        model = split_name[1]
+        arch, patch_size = model[3], int(model[4:])
+        feat_dim_lookup = {"s": 384, "b": 768, "l": 1024, "g": 1536}
+        feat_dim: int = feat_dim_lookup[arch]
+        return feat_dim, patch_size
+
+    def compute_shift_directions(self, pattern: Neighbourhood) -> List[Tuple[int, int]]:
+        # Precompute neighbourhood shift unit vectors
+        shifts = [  # shifts in yx format
+            (-1, -1),
+            (-1, 0),
+            (-1, 1),
+            (0, -1),
+            (0, 0),
+            (0, 1),
+            (1, -1),
+            (1, 0),
+            (1, 1),
+        ]
+        shift_directions: List[Tuple[int, int]] = []
         for i in range(9):
-            # We need (out_ch, in_ch, h, w) for our weights dimension
-            zero_mask = torch.zeros((3, 1, 3, 3))
             if pattern == "Neumann" and i % 2 == 1:
-                zero_mask[:, :, i // 3, i % 3] = 1
-                out_masks.append(zero_mask)
-            elif pattern == "Moore":
-                zero_mask[:, :, i // 3, i % 3] = 1
-                out_masks.append(zero_mask)
-        return out_masks
+                shift_directions.append(shifts[i])
+            elif pattern == "Moore" and i != 4:
+                shift_directions.append(shifts[i])
+        return shift_directions
 
+    @torch.no_grad()
+    def low_res_features(self, x: torch.Tensor) -> torch.Tensor:
+        feat_dict = self.dinov2.forward_features(x)
+        feat_tensor: torch.Tensor = feat_dict["x_norm_patchtokens"]
+        if self.dtype != torch.float32:
+            feat_tensor = feat_tensor.type(self.dtype)
+        return feat_tensor
+
+    @torch.no_grad()
     def get_shifted_img_batch(self, x: torch.Tensor) -> torch.Tensor:
-        # For each pixel shift and each direction in mask, compute conv2d of img with mask and store it
+        # For each shift direction and distance, torch.roll image for pixel shift w/ circular bcs
         img_list: List[torch.Tensor] = []
-        for m in self.masks:
-            for s in self.shifts:
-                padded: torch.Tensor = F.pad(
-                    x.unsqueeze(0), (s, s, s, s), mode="circular"
-                )
-                shifted_img = F.conv2d(
-                    padded, m, stride=1, dilation=s, groups=3
-                )  # groups=3
+        for shift_dir in self.shift_directions:
+            for shift_dist in self.shift_distances:
+                shift = (shift_dir[0] * shift_dist, shift_dir[1] * shift_dist)
+                shifted_img: torch.Tensor = torch.roll(x, shift, dims=(-2, -1))
                 img_list.append(shifted_img)
-        img_batch = torch.cat(img_list)
+        img_batch = torch.stack(img_list)
+        if self.dtype != torch.float32:
+            img_batch = img_batch.type(self.dtype)
         return img_batch
 
-    def pca(self, f: torch.Tensor, norm: bool = False) -> torch.Tensor:
-        # TODO: test if pytorhc's batched implementation would work better?
-        # Use pytorch's PCA on x to get first k components. Data is not reshaped.
-        projection = self.PCA.fit_transform(f)
-        if norm:
-            projection = F.normalize(projection)
+    @torch.no_grad()
+    def pca(self, f: torch.Tensor, k: int) -> torch.Tensor:
+        # Approximate (batched) tensor f with its k principal components computed over all b in f
+        U, S, V = torch.pca_lowrank(f, q=k)
+        projection = torch.matmul(f, V)
         return projection
 
+    @torch.no_grad()
     def rescale_unshift_features(
         self, feature_batch: torch.Tensor, x: torch.Tensor
     ) -> torch.Tensor:
         _, h, w = x.shape
-        c = FEAT_DIM if self.n_components < 1 else self.n_components
+        c = self.feat_dim if self.pca_each is False else self.pca_k
+        n_patch_h, n_patch_w = h // self.patch_size, w // self.patch_size
+
         # Summand variable here to be memory efficient
-        out_feature_img: torch.Tensor = torch.zeros(1, c, h, w, device=x.device)
+        out_feature_img: torch.Tensor = torch.zeros(
+            1, c, h, w, device=x.device, dtype=self.dtype
+        )
+
         feat_idx: int = 0
-        # TODO: make sure this reverse ordering works for moore neighbourhoods
-        for m in self.masks[::-1]:
-            for s in self.shifts:
+        for shift_dir in self.shift_directions:
+            for shift_dist in self.shift_distances:
                 feat_patch_flat = feature_batch[feat_idx]
-                # should I PCA before averaging or after?
-                if self.n_components < 1:
-                    reduced_dim_flat = feat_patch_flat
-                else:
-                    reduced_dim_flat = self.pca(feat_patch_flat)
-                feat_patch = reduced_dim_flat.reshape((PATCH_H, PATCH_W, c))
-                feat_img: torch.Tensor = torch.kron(
-                    feat_patch, torch.ones((14, 14, 1), device=x.device)
-                )
-                full_size_tensor = feat_img.permute((2, 0, 1))
-                # TODO: can i replace the kron with an interpolate
-                # and/or can i get rid of all these resizes
-                # feat_img: torch.Tensor = F.interpolate(feat_patch, size=(h, w))
-                padded = F.pad(
-                    full_size_tensor.unsqueeze(0), (s, s, s, s), mode="circular"
-                )
-                spatial_mask = m[0, 0, :, :]
-                channel_mask = spatial_mask.unsqueeze(0).unsqueeze(0)
-                # we want a depthwise convolution here i.e each bit of the input gets
-                # its own filter and mapped to its own output channel
-                channel_mask = channel_mask.repeat(c, 1, 1, 1)
-                unshifted = F.conv2d(
-                    padded, channel_mask, stride=1, dilation=s, groups=c
+                if self.pca_each:
+                    feat_patch_flat = self.pca(feat_patch_flat, self.pca_k)
+                # reshape or view - view possibly more memory efficient
+                feat_patch = feat_patch_flat.view((n_patch_h, n_patch_w, c))
+                permuted = feat_patch.permute((2, 0, 1)).unsqueeze(0)
+
+                full_size_tensor = F.interpolate(
+                    permuted, scale_factor=self.patch_size, mode="nearest-exact"
                 )
 
+                rev_shift = (
+                    -1 * shift_dir[0] * shift_dist,
+                    -1 * shift_dir[1] * shift_dist,
+                )
+                unshifted = torch.roll(full_size_tensor, rev_shift, dims=(-2, -1))
                 out_feature_img += unshifted
                 feat_idx += 1
 
@@ -200,26 +133,9 @@ class HighResDV2(nn.Module):
         mean = out_feature_img / n_imgs
         return mean
 
+    @torch.no_grad()
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        x.detach()
-        with torch.no_grad():
-            img_batch = self.get_shifted_img_batch(x)
-            features_dict = self.dinov2.forward_features(img_batch)
-            features_batch: torch.Tensor = features_dict["x_norm_patchtokens"]
-            upsampled_unshifted = self.rescale_unshift_features(features_batch, x)
-            return upsampled_unshifted
-
-
-"""
-plane_tensor, plane_img = load_image(f"{PATH}/dv2/plane.jpg", transform)
-plane_tensor = plane_tensor.to("cuda")
-print(plane_tensor.shape)
-
-shifts = [2, 4, 7]  # [i for i in range(1, 8)]
-net = HighResDV2("dinov2_vits14", shifts)
-net.cuda()
-
-features = net(plane_tensor)
-
-print(features.shape)
-"""
+        img_batch = self.get_shifted_img_batch(x)
+        features_batch = self.low_res_features(img_batch)
+        upsampled_unshifted = self.rescale_unshift_features(features_batch, x)
+        return upsampled_unshifted
