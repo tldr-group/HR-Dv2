c8525efc9d5b23b3273bc40fd7135a5bbcb2c172
rmdocherty <rmdocherty2@gmai.com>
2023-10-20 15:51:44 +0100

    Updated automatic thresholding notebook

--- dv2/utils.py
+++ dv2/utils.py
@@ -4,7 +4,10 @@ from PIL import Image
 import numpy as np
 from sklearn.decomposition import PCA
 
-from typing import Tuple
+from skimage.filters.rank import entropy
+from skimage.morphology import disk
+
+from typing import Tuple, List
 
 
 def get_input_transform(resize_dim: int, crop_dim: int) -> transforms.Compose:
@@ -49,6 +52,16 @@ def to_numpy(x: torch.Tensor, batched: bool = True) -> np.ndarray:
     if batched:
         x = x.squeeze(0)
     return x.detach().cpu().numpy()
+
+
+def flatten(
+    x: torch.Tensor | np.ndarray, h: int, w: int, c: int, convert: bool = False
+) -> np.ndarray:
+    if type(x) == torch.Tensor:
+        x = to_numpy(x)
+    x = x.reshape((c, h * w))
+    x = x.T
+    return x
 
 
 def do_single_pca(arr: np.ndarray, n_components: int = 3) -> np.ndarray:
@@ -123,3 +136,32 @@ def threshold_pca(
         fg_mask = np.where(pca[:, 0] < threshold, 1, 0)
     fg_features: np.ndarray = features[fg_pca]
     return fg_features, fg_pca, fg_mask
+
+
+def get_entropy_img(arr: np.ndarray, k: int = 10) -> np.ndarray:
+    return entropy(arr, disk(k))
+
+
+def entropy_per_area(mask: np.ndarray, entropy_img: np.ndarray) -> float:
+    n_fg: int = np.sum(mask)
+    total_entropy = np.sum(entropy_img * mask)
+    return total_entropy / n_fg
+
+
+def get_best_mask(
+    feat_arr: np.ndarray, grey_img_arr: np.ndarray, k: int = 10
+) -> Tuple[np.ndarray, int]:
+    h, w = grey_img_arr.shape
+    entropy_img = get_entropy_img(grey_img_arr, k)
+
+    pca = do_single_pca(feat_arr)
+    mask: np.ndarray
+    _feat, _pca, mask = threshold_pca(feat_arr, pca, 0, True, True)
+    mask = mask.reshape(h, w)
+    masks: List[np.ndarray] = [mask, ~mask]
+
+    threshold_1_entropy = entropy_per_area(mask, entropy_img)
+    threshold_2_entropy = entropy_per_area(~mask, entropy_img)
+    best_mask_idx: int = np.argmax([threshold_1_entropy, threshold_2_entropy])
+    best_mask = masks[best_mask_idx]
+    return best_mask, best_mask_idx
